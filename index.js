89d95fc60a902af0f4e803abefaccb78bfe5671d
Joyce <joycelin79@gmail.com>
2020-04-30 16:02:36 -0700

    add getTaskbyId

--- index.js
+++ index.js
@@ -9,15 +9,14 @@ app.listen(3000, () => {
 app.get("/spacestation", (req, res, next) => {
     res.json([
         "wash the spaceship",
-        "clean the space kitchen", 
+        "clean the space kitchen",
         "look at the stars",
         "repair my space suit"
     ]);
 });
 
 app.get("/tasks", db.getTasks)
-// app.get("/tasks/:id", db.getTaskById)
+app.get("/tasks/:id", db.getTaskById)
 // app.post("/tasks", db.createTask)
 // app.put("/tasks/:id", db.updateTask)
 // app.delete("/tasks/:id", db.deleteTasks)
-    
--- model.js
+++ model.js
@@ -18,11 +18,20 @@ const getTasks = (request, response) => {
         response.status(200).json(results.rows)
     })
 }
+const getTaskById = (request, response) => {
+    const id = parseInt(request.params.id)
+    client.query('SELECT * FROM tasks WHERE id = $1', [id], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
 
 
 module.exports = {
-    getTasks
-    // getTaskById,
+    getTasks,
+    getTaskById
     // createTask,
     // updateTask,
     // deleteTask,

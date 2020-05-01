ac4533e402643a9cb69b929287751cfad7b3226a
Joyce <joycelin79@gmail.com>
2020-05-01 09:44:11 -0700

    add other routes

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-var express = require("express");
-var app = express();
-var db = require("./model");
+const express = require("express")
+const app = express()
+const port = 3000
+const db = require("./model")
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
-});
+const bodyParser = require('body-parser')
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`)
+})
 
 app.get("/spacestation", (req, res, next) => {
     res.json([
@@ -12,11 +17,11 @@ app.get("/spacestation", (req, res, next) => {
         "clean the space kitchen",
         "look at the stars",
         "repair my space suit"
-    ]);
-});
+    ])
+})
 
 app.get("/tasks", db.getTasks)
 app.get("/tasks/:id", db.getTaskById)
-// app.post("/tasks", db.createTask)
-// app.put("/tasks/:id", db.updateTask)
-// app.delete("/tasks/:id", db.deleteTasks)
+app.post("/tasks", db.createTask)
+app.put("/tasks/:id", db.updateTask)
+app.delete("/tasks/:id", db.deleteTask)
--- model.js
+++ model.js
@@ -1,4 +1,4 @@
-const { Client } = require('pg');
+const { Client } = require('pg')
 
 const client = new Client({
     // user: 'username',
@@ -11,28 +11,58 @@ const client = new Client({
 client.connect()
 
 const getTasks = (request, response) => {
-    client.query('SELECT * FROM tasks ORDER BY id ASC', (error, results) => {
+    client.query('SELECT * FROM tasks ORDER BY id ASC', (error, result) => {
         if (error) {
             throw error
         }
-        response.status(200).json(results.rows)
+        response.status(200).json(result.rows)
     })
 }
 const getTaskById = (request, response) => {
     const id = parseInt(request.params.id)
-    client.query('SELECT * FROM tasks WHERE id = $1', [id], (error, results) => {
+    client.query('SELECT * FROM tasks WHERE id = $1', [id], (error, result) => {
         if (error) {
             throw error
         }
-        response.status(200).json(results.rows)
+        response.status(200).json(result.rows)
     })
 }
 
+const createTask = (request, response) => {
+    const task = request.body.message
+    client.query('INSERT INTO tasks (task) VALUES ($1) RETURNING *', [task], (error, result) => {
+        if (error) {
+            throw error
+        }
+        response.status(201).send(`Task added: ID ${result.rows[0].id}`)
+    })
+}
+
+const updateTask = (request, response) => {
+    const id = parseInt(request.params.id)
+    const task = request.body.message
+    client.query('UPDATE tasks SET task = $1 WHERE id = $2 RETURNING *', [task, id], (error, result) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).send(`Task updated: ID ${result.rows[0].id}`)
+    })
+}
+
+const deleteTask = (request, response) => {
+    const id = parseInt(request.params.id)
+    client.query('DELETE FROM tasks WHERE id = $1 RETURNING *', [id], (error, result) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).send(`Task deleted: ID ${result.rows[0].id}`)
+    })
+}
 
 module.exports = {
     getTasks,
-    getTaskById
-    // createTask,
-    // updateTask,
-    // deleteTask,
+    getTaskById,
+    createTask,
+    updateTask,
+    deleteTask
 }
